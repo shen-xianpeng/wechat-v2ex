084d35fd9057e8a626ce19244f530ec81a7b2099
xianpeng <xianpeng@xianpeng.here>
2017-05-21 00:05:35 +0800

    加载更多

--- pages/bookList/bookList.js
+++ pages/bookList/bookList.js
@@ -8,7 +8,7 @@ Page({
     datalist: [],
     hidden: false,
     scrollTop: 0,
-    scrollHeight: 0  
+    scrollHeight: 0,
   },
   onLoad: function () {
     var that = this;
@@ -41,16 +41,29 @@ Page({
   },
   fetchData: function(callback) {
     var that = this;
+    var params = {};
+    var data = [];
+    if (this.data.offset) {
+      data = this.data.datalist
+      params["offset"] = this.data.offset
+    }
     wx.request({
       url: "http://118.184.11.37:10000/book_list",
+      data: params,
       success: function(res) {
           if (callback) {
             callback();
           }
+          var datas = data.concat(res.data.infos);
           that.setData({
             hidden: true,
-            datalist: res.data.infos
+            datalist: datas,
+            loadingBottom: false,
+            hasMore: res.data.has_more,
+            offset: res.data.offset,
           })
+
+
       }
     })
   },
@@ -60,6 +73,8 @@ Page({
 
    that.setData({
      hidden: true,
+     hasMore: undefined,
+     offset: undefined,
      //datalist: []
    })
    wx.showToast({
@@ -81,5 +96,13 @@ Page({
   },
   onReachBottom: function () {
     console.log("上拉加载更多...");
+    if (this.data.hasMore==false) {
+      console.log("没有更多了")
+      return
+    }
+    this.setData({
+      loadingBottom: true,
+    })
+    this.fetchData();
   }
 })
