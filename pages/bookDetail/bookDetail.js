15eb4b78e8c993572a718b78e9df93a07b4aa322
xianpeng <xianpeng@xianpeng.here>
2017-05-30 21:41:46 +0800

    scroll swipper tab

--- pages/bookDetail/bookDetail.js
+++ pages/bookDetail/bookDetail.js
@@ -5,6 +5,7 @@ var Api = require('../../utils/api.js');
 Page({
   data: {
     title: '书本详情',
+    currentTab: 0,
     book: {},
   },
   fetchDetail: function (id) {
@@ -23,7 +24,8 @@ Page({
     that.fetchReplies(id);
   },
   fetchReplies: function (id) {
-  
+    //http://www.webhek.com/post/css-100-percent-height.html
+    //块级元素
   },
   onPreviewImage: function (e) {
     console.log(e.currentTarget.id);
@@ -31,6 +33,12 @@ Page({
       current: e.currentTarget.id, // 当前显示图片的http链接
       urls: [e.currentTarget.id] // 需要预览的图片http链接列表
     })
+  },
+  clickTab: function (e) {
+    console.log(e.currentTarget.dataset.current)
+    var that = this;
+    that.setData({ currentTab: e.currentTarget.dataset.current });
+
   },
   onLoad: function (options) {
     console.log(options);
