cdc2b117e7e75044431e488d3dfbcad487dfb00c
shenxianpeng <shen.xianpeng@outlook.com>
2017-06-07 15:20:30 +0800

    add

--- pages/addBook/addBook.js
+++ pages/addBook/addBook.js
@@ -6,9 +6,9 @@ Page({
     title: '添加图书',
     book: {},
     hidden: false,
-    image_list : [],
-    book_photos : [
-      {id:1},{id:2},{id:3}
+    image_list: [],
+    book_photos: [
+      { id: 1, label: "封面" }, { id: 2, label: "背面" }, { id: 3, label: "更多(可选多张)" }
     ],
     showSuccessModal: false,
     showErrorModal: false,
@@ -46,9 +46,9 @@ Page({
             //   })
             //   return;
             // }
-            // that.setData({
-            //   book: res.data
-            // })
+            that.setData({
+              book: res.data
+            })
             // that.book_id = res.data.id;
             // that.setData({
             //   showSuccessModal: true
@@ -67,13 +67,13 @@ Page({
     })
   },
   chooseImages: function (e) {
-    var that=this;
+    var that = this;
     console.log(e.currentTarget.dataset.id);
-    var count=9;
-    if (e.currentTarget.dataset.id<3) {
-      count=1;
+    var count = 9;
+    if (e.currentTarget.dataset.id < 3) {
+      count = 1;
     } else {
-      count = 9+1 - e.currentTarget.dataset.id;
+      count = 9 + 1 - e.currentTarget.dataset.id;
     }
     wx.chooseImage({
       count: count, // 默认9
@@ -86,50 +86,52 @@ Page({
         that.setData({
           image_list: tempFilePaths
         });
-        for (var i= 0; i < tempFilePaths.length; i++) {
-          console.log("start....", tempFilePaths[i], e.currentTarget.dataset.id + i )
+        for (var i = 0; i < tempFilePaths.length; i++) {
+          console.log("start....", tempFilePaths[i], e.currentTarget.dataset.id + i)
           var tmpData = that.data.book_photos;
-          if (tmpData.length < e.currentTarget.dataset.id + i ) {
+          if (tmpData.length < e.currentTarget.dataset.id + i) {
             tmpData.push({
               id: e.currentTarget.dataset.id + i
             })
           }
-      
+
           tmpData[e.currentTarget.dataset.id + i - 1].path = tempFilePaths[i];
-          that.setData({ book_photos:tmpData});
-          function as ( tmpI ){
-          wx.uploadFile({
-            url: 'https://www.xianpeng.org/upload_file', //仅为示例，非真实的接口地址
-            filePath: tempFilePaths[i],
-            name: 'file',
-            formData: {
-            },
-            success: function (res) {
-              var data = res.data
-              var tmpData = that.data.book_photos;
-              console.log("set", e.currentTarget.dataset.id + tmpI - 1, tmpData.length, tmpI)
-              tmpData[e.currentTarget.dataset.id + tmpI - 1].src = tempFilePaths[tmpI];
-              that.setData({ book_photos: tmpData });
-            },
-            fail: function(e) {
-              console.log(e);
-            },
-            complete: function () {
-              console.log("complete");
-            }
-          })
-        }(i)
+          that.setData({ book_photos: tmpData });
+          function as(tmpI) {
+            console.log("upload....", tmpI)
+            wx.uploadFile({
+              url: 'https://www.xianpeng.org/upload_file', //仅为示例，非真实的接口地址
+              filePath: tempFilePaths[tmpI],
+              name: 'file',
+              formData: {
+              },
+              success: function (res) {
+                var data = res.data
+                var tmpData = that.data.book_photos;
+                console.log("set", e.currentTarget.dataset.id + tmpI - 1, tmpData.length, tmpI)
+                tmpData[e.currentTarget.dataset.id + tmpI - 1].src = data;
+                that.setData({ book_photos: tmpData });
+              },
+              fail: function (e) {
+                console.log(e);
+              },
+              complete: function () {
+                console.log("complete");
+              }
+            })
+          };
+          as(i)
         }
         var tmpData = that.data.book_photos;
 
-        if (e.currentTarget.dataset.id>2 && tmpData.length < 9) {
+        if (e.currentTarget.dataset.id > 2 && tmpData.length < 9) {
           tmpData.push({
             id: tmpData.length + 1
           })
         }
         that.setData({ book_photos: tmpData });
 
-       
+
       }
     })
   },
@@ -170,14 +172,14 @@ Page({
   },
   onLoad: function (options) {
     this.fetchData(options.id);
-wx.showActionSheet({
-  itemList: ['A', 'B', 'C'],
-  success: function(res) {
-    console.log(res.tapIndex)
-  },
-  fail: function(res) {
-    console.log(res.errMsg)
-  }
-})
+    // wx.showActionSheet({
+    //   itemList: ['A', 'B', 'C'],
+    //   success: function(res) {
+    //     console.log(res.tapIndex)
+    //   },
+    //   fail: function(res) {
+    //     console.log(res.errMsg)
+    //   }
+    // })
   }
 })
