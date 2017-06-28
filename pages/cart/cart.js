5752412b0418be92ffed7f6ab8efc5790f9406e4
shenxianpeng <shen.xianpeng@outlook.com>
2017-06-28 17:02:10 +0800

    pages

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -5,7 +5,8 @@ Page({
   data: {
     groupList: []
   },
-  getCartInfo: function (e) {
+  getCartInfo: function (callback) {
+    var callback = callback;
     var that = this;
     wx.request({
       url: getApp().config.host + '/cart_item_list',
@@ -19,11 +20,37 @@ Page({
         that.setData({
           groupList: res.data.data
         })
+      },
+      complete: function () {
+        callback();
       }
     });
   },
   onLoad: function (options) {
     console.log(options);
     this.getCartInfo(options.id);
+  },
+  onPullDownRefresh: function () {
+    console.log("下拉刷新");
+    var that = this;
+    wx.stopPullDownRefresh();
+
+
+    wx.showNavigationBarLoading()
+
+    that.getCartInfo(function () {
+      wx.hideNavigationBarLoading();   
+      wx.stopPullDownRefresh({
+        complete: function (res) {
+
+          console.log(res, new Date())
+        }
+      })  
+    })
+
+  },
+  stopPullDownRefresh: function () {
+
+   
   },
 })
